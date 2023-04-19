1a4116b1a90ee4cfd5287045bc93dae6dca98e4f
Ryan Kelly <ryan@kiiii.com>
2023-04-19 18:28:54 +1000

    Update examples true_dgp

--- rsnl/examples/misspec_ma1.py
+++ rsnl/examples/misspec_ma1.py
@@ -40,6 +40,32 @@ def get_prior():
                         high=jnp.array([1.0]))
 
 
+# TODO: TERRIBLE IMPLEMENTATION ... RUN ONCE AT START SO WHO CARES
+# @partial(jit, static_argnums=(3,4))
+def true_dgp(w=-0.736, rho=0.9, sigma_v=0.36, batch_size=1, n_obs=100, key=None):
+    """Generate data from true DGP."""
+    h_mat = jnp.zeros((batch_size, n_obs))
+    y_mat = jnp.zeros((batch_size, n_obs))
+
+    w_vec = jnp.repeat(w, batch_size)
+    rho_vec = jnp.repeat(rho, batch_size)
+    sigma_v_vec = jnp.repeat(sigma_v, batch_size)
+
+    key, subkey = random.split(key)
+    h_mat = h_mat.at[:, 0].set(w_vec + dist.Normal(0, 1).sample(key=subkey, sample_shape=(batch_size,)) * sigma_v_vec)
+    key, subkey = random.split(key)
+    y_mat = y_mat.at[:, 0].set(jnp.exp(h_mat[:, 0]/2) * dist.Normal(0, 1).sample(key=subkey, sample_shape=(batch_size,)))
+
+    # TODO?
+    for i in range(1, n_obs):
+        key, subkey = random.split(key)
+        h_mat = h_mat.at[:, i].set(w_vec + rho_vec * h_mat[:, i-1] + dist.Normal(0, 1).sample(key=subkey, sample_shape=(batch_size,)) * sigma_v_vec)
+        key, subkey = random.split(key)
+        y_mat = y_mat.at[:, i].set(jnp.exp(h_mat[:, i]/2)*dist.Normal(0, 1).sample(key=subkey, sample_shape=(batch_size,)))
+
+    return y_mat
+
+
 def true_posterior(x_obs: jnp.ndarray,
                    prior: dist.Distribution) -> jnp.ndarray:
     """Return true posterior for misspecified MA(1) example."""
